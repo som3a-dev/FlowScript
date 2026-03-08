ee3095d79912066c1b70d182c2e84d6e060a011c
som3a-dev <omar.eltayeb54@gmail.com>
2026-03-09 01:56:26 +0200

    Hello World

---
+++ source/main.c
@@ -0,0 +1,14 @@
+﻿/*
+ * File: main.c
+ * Created on Mon Mar 09 2026
+ *
+ * Copyright (c) 2026 Omar Eltayeb
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+	printf("Hello FlowScript");
+	return 0;
+}
